a1dd8bc552529267f7b32434130a8b883046a429
Aditya Pandey <adityapandey2395@gmail.com>
2024-06-08 10:33:39 +0530

    Added Birthday and onclick handler

--- app/src/main/java/com/example/my_app/MainActivity.kt
+++ app/src/main/java/com/example/my_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.my_app
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-click-handler","button clicked")
     }
 }
